87b012e63c751a0860bad9266a07da0c52c2fa5a
Alejo Warner <alewar2783@gmail.com>
2026-08-04 17:22:31 -0300

    Wwise

--- TopDownShooter_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ TopDownShooter_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,13 +13,36 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_AUDIOCLEANER_HIDOI_NA_HITO_NO_KOKORO_TOKA_NAI_KA_20260802_224656 = 2165242676U;
+        static const AkUniqueID BOSS_THEME = 2166191340U;
+        static const AkUniqueID BOX_DESTRUCTION = 610178761U;
+        static const AkUniqueID BUTTON_CLICKED = 1830902963U;
+        static const AkUniqueID BUTTON_HOVER = 1701177942U;
+        static const AkUniqueID BUTTON_PLAY = 1394085446U;
+        static const AkUniqueID ENEMY_1_ATTACK = 1476537150U;
+        static const AkUniqueID ENEMY_1_DEATH = 1623256164U;
+        static const AkUniqueID ENEMY_1_HURT = 1832299857U;
+        static const AkUniqueID ENEMY_2_ATTACK = 514384443U;
+        static const AkUniqueID ENEMY_2_DEATH = 877981099U;
+        static const AkUniqueID ENEMY_2_HURT = 1781786948U;
+        static const AkUniqueID LOOT_HEALTH = 2298755096U;
+        static const AkUniqueID LOOT_MONEY = 646549102U;
+        static const AkUniqueID LOOT_OBJECT = 2259005123U;
+        static const AkUniqueID LOOT_POTION = 1827792103U;
+        static const AkUniqueID MENU_MUSIC = 4055567060U;
+        static const AkUniqueID PLAYER_ATTACK_AXE = 3105051176U;
+        static const AkUniqueID PLAYER_ATTACK_DAGGER = 3172158790U;
+        static const AkUniqueID PLAYER_ATTACK_STAFF = 2906993614U;
+        static const AkUniqueID PLAYER_DASH = 2394582229U;
+        static const AkUniqueID PLAYER_DEATH = 3083087645U;
+        static const AkUniqueID THEME_1 = 1805655660U;
+        static const AkUniqueID THEME_2 = 1805655663U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID SOUNDBANK_ENEMIGOS = 3134885082U;
+        static const AkUniqueID SOUNDBANK_TOPDOWN_GAMEPLAY = 1114115615U;
+        static const AkUniqueID SOUNDBANK_TOPDOWN_UI_MENUS = 1507514792U;
     } // namespace BANKS
 
     namespace BUSSES
